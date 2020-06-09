0f54c69038c5f990ad59e9d81349b491d80e5f97
NICOLE KOO JIA YING <19047433@myrp.edu.sg>
2020-06-09 20:44:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19047433/arraylist/MainActivity.java
@@ -0,0 +1,34 @@
+package sg.edu.rp.c346.id19047433.arraylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    ArrayList<String> fruits;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add(3, "Durian");
+        String theFruit = fruits.get(2);
+        fruits.remove(0);
+        fruits.set(3, "Dragon Fruit");
+        String text = "Fruits\n";
+        text += "=====\n";
+        for(int i = 0; i < fruits.size(); i++){
+            text += fruits.get(i) + "\n";
+        }
+        tv.setText(text);
+    }
+}
